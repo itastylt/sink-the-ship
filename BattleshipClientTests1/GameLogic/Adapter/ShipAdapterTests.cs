b6c10d978b7208666732c6a18466145fe518b1c1
Paulius <40724700+jungassuo@users.noreply.github.com>
2023-11-30 20:51:42 +0200

    added generated tests

---
+++ BattleshipClientTests1/GameLogic/Adapter/ShipAdapterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleshipClient.GameLogic.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipClient.GameLogic.Adapter.Tests
+{
+    [TestClass()]
+    public class ShipAdapterTests
+    {
+        [TestMethod()]
+        public void FireTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ BattleshipClientTests1/GameLogic/Invokers/CloneShipTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleshipClient.GameLogic.Invokers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipClient.GameLogic.Invokers.Tests
+{
+    [TestClass()]
+    public class CloneShipTests
+    {
+        [TestMethod()]
+        public void CloneShipTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void executeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void undoAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
