2aa053c19b3ab8ceffc5b8266d1ec177ea887dfe
itastylt <100284073+itastylt@users.noreply.github.com>
2023-11-30 20:42:53 +0200

    FacadeTests

---
+++ BattleshipClientTests1/GameLogic/Facade/ReadyFacadeTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ReadyFacadeTests
+    {
+        [TestMethod()]
+        public void ReadyFacadeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FormBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateRandomPlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartPlayersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UnreadyPlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RestorePlayerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
